6f1920cd70faddf29258f33bb08dfe790ba7ec3e
Gabriel Carlos <gabrielcarlos91@hotmail.com>
2024-06-25 19:25:02 -0300

    fix: redis connection

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,17 +1,12 @@
 import { createClient, RedisClientType } from "redis";
-import env from "../env";
 
 export default class Redis {
 	private static instance: Redis;
 	private client: RedisClientType;
 
 	private constructor() {
-		const url =
-			env.NODE_ENV !== "production"
-				? "redis://localhost:6379"
-				: "redis://redis:6379";
 		this.client = createClient({
-			url,
+			url: "redis://redis:6379",
 		});
 
 		this.client.on("error", (err) => console.log("Redis Client Error", err));
