2ff778fb766295cb4883ba514731335646e75126
Gabriel Carlos <gabrielcarlos91@hotmail.com>
2024-06-30 23:56:09 -0300

    fix: script redis connection

--- update_meta.ts
+++ update_meta.ts
@@ -1,25 +1,36 @@
 import { metaHeroesType } from "./src/constants/index";
 import { D2PtScraper } from "d2pt.js";
-import RedisManager from "./src/utils/redis-manager";
 
 import * as dotenv from "dotenv";
+import { createClient } from "redis";
 
 dotenv.config();
 
 const ROLES = ["HC", "MID", "TOP", "SUP4", "SUP5"];
 const d2pt = new D2PtScraper();
-const redisManager = new RedisManager();
+
+const redisData = {
+	host: "127.0.0.1",
+	port: process.env.REDIS_PORT,
+	password: process.env.REDIS_PASSWORD,
+};
+const redisUrl = `redis://default:${redisData.password}@${redisData.host}:${redisData.port}`;
+const redisClient = createClient({
+	url: redisUrl,
+});
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+redisClient.connect();
 
 async function getHeroesMetaWithDelay(index: number) {
 	if (index < ROLES.length) {
 		try {
 			const ROLE = ROLES[index].toLocaleLowerCase() as metaHeroesType;
-			const getHeroesMetaCache = await redisManager.get(ROLE);
+			const getHeroesMetaCache = await redisClient.get(ROLE);
 			if (!getHeroesMetaCache) {
 				const heroesMeta = await d2pt.getHeroesMeta(ROLE);
 				console.log(`Fetched data for ${ROLES[index]}`);
 				console.log(heroesMeta);
-				await redisManager.set(ROLES[index], JSON.stringify(heroesMeta));
+				await redisClient.set(ROLES[index], JSON.stringify(heroesMeta));
 			}
 		} catch (error) {
 			console.error(`Error fetching heroes meta for ${ROLES[index]}:`, error);
