973e90a8e534eaf1489f146cf9e28987db6e978e
Gabriel Carlos <gabrielcarlos91@hotmail.com>
2024-09-26 20:41:59 -0300

    fix: update meta shell

--- update_meta.sh
+++ update_meta.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-/usr/bin/ts-node /root/twitch-chat-dota2/update_meta.ts
+# update_meta.sh
+echo "Current working directory:"
+pwd  # Mostra o diretório de trabalho atual
+
+echo "Path to the script:"
+realpath update_meta.sh  # Mostra o caminho absoluto para o script
+
+# Executa o arquivo .ts
+ts-node /root/twitch-chat-dota2/update_meta.ts
