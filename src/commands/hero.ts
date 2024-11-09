1d99f5aca309a0d4801ce6631c6005ff6f495456
Gabriel Carlos <gabrielcarlos91@hotmail.com>
2024-11-09 11:40:38 -0300

    fix: command get hero info

--- src/commands/hero.ts
+++ src/commands/hero.ts
@@ -8,63 +8,63 @@ const d2pt = new D2PtScraper();
 const redisManager = new RedisManager();
 
 function isHeroName(name: string): string | null {
-	const normalizedName = name.toLowerCase();
-	for (const [hero, aliases] of Object.entries(heroesAliasses)) {
-		if (aliases.includes(normalizedName)) {
-			return hero ?? null;
-		}
-	}
-	return null;
+  const normalizedName = name.toLowerCase();
+  for (const [hero, aliases] of Object.entries(heroesAliasses)) {
+    if (aliases.includes(normalizedName)) {
+      return hero ?? null;
+    }
+  }
+  return null;
 }
 
 export default new Command(
-	"hero",
-	async ({ rawArgs, client, channel, tags }) => {
-		const args = rawArgs.split(" ");
-		const heroName: HeroNames = args[0].toLocaleLowerCase() as HeroNames;
-		let response = "";
-		let heroinfo = null;
-		const heroMatch = isHeroName(heroName);
+  "hero",
+  async ({ rawArgs, client, channel, tags }) => {
+    const args = rawArgs.split(" ");
+    const heroName: HeroNames = args[0].toLocaleLowerCase() as HeroNames;
+    let response = "";
+    let heroinfo = null;
+    const heroMatch = isHeroName(heroName);
 
-		if (heroMatch) {
-			const heroSelected = Object.keys(HeroNames).find(
-				(hero) => hero.toLowerCase() === heroMatch.toLocaleLowerCase()
-			);
-			const heroValue = heroSelected
-				? HeroNames[heroSelected as keyof typeof HeroNames]
-				: null;
-			const getHeroInfoCache = await redisManager.get(heroSelected ?? "");
-			if (!getHeroInfoCache) {
-				const heroInfoData = await d2pt.getHeroInfo(heroValue as HeroNames);
-				heroinfo = heroInfoData
-					?.filter((hero) => hero.role?.includes("Most Played"))
-					.map((hero) => ({
-						role: hero.role,
-						matches: hero.matches,
-						winRate: hero.winRate,
-					}))[0];
-				if (!heroinfo) {
-					client.say(
-						channel,
-						`@${
-							tags.username
-						}, No data found for ${heroName.toUpperCase()}, please try again later`
-					);
-					return;
-				}
-				await redisManager.set(heroSelected ?? "", JSON.stringify(heroinfo));
-			} else {
-				heroinfo = JSON.parse(getHeroInfoCache);
-			}
+    if (heroMatch) {
+      const heroSelected = Object.keys(HeroNames).find(
+        (hero) => hero.toLowerCase() === heroMatch.toLocaleLowerCase()
+      );
+      const heroValue = heroSelected
+        ? HeroNames[heroSelected as keyof typeof HeroNames]
+        : null;
+      const getHeroInfoCache = await redisManager.get(heroSelected ?? "");
+      if (!getHeroInfoCache) {
+        const heroInfoData = await d2pt.getHeroInfo(heroValue as HeroNames);
+        heroinfo = heroInfoData
+          ?.filter((hero) => hero.mostPlayed?.includes("Most Played"))
+          .map((hero) => ({
+            role: hero.role,
+            matches: hero.matches,
+            winRate: hero.winRate,
+          }))[0];
+        if (!heroinfo) {
+          client.say(
+            channel,
+            `@${
+              tags.username
+            }, No data found for ${heroName.toUpperCase()}, please try again later`
+          );
+          return;
+        }
+        await redisManager.set(heroSelected ?? "", JSON.stringify(heroinfo));
+      } else {
+        heroinfo = JSON.parse(getHeroInfoCache);
+      }
 
-			response = ``;
-			response += `⭐ ${heroName.toUpperCase()} - ${
-				heroinfo?.role
-			} | Win Rate: ${heroinfo?.winRate} | Matches: ${heroinfo?.matches}`;
-		} else {
-			response = "Hero name not found!";
-		}
+      response = ``;
+      response += `⭐ ${heroName.toUpperCase()} - ${
+        heroinfo?.role
+      } | Win Rate: ${heroinfo?.winRate} | Matches: ${heroinfo?.matches}`;
+    } else {
+      response = "Hero name not found!";
+    }
 
-		client.say(channel, `@${tags.username}, ${response}`);
-	}
+    client.say(channel, `@${tags.username}, ${response}`);
+  }
 );
